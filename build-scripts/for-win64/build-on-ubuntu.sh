f3ac29a4b95fc7cbebf0b8060dd10c5384a257b8
Oleg Samarin <osamarin68@gmail.com>
2021-09-23 11:55:20 +0300

    Using variable MINGW64_PREFIX

--- build-scripts/for-win64/build-on-ubuntu.sh
+++ build-scripts/for-win64/build-on-ubuntu.sh
@@ -6,7 +6,7 @@ SRC_DIR=`dirname $0`/../..
 PARALLEL_PRMS="-j$(nproc)"
 DEBIAN_PKG_NAME=wxwidgets3.0-mingw-w64
 VERSION=${1:-3.0.2}
-BUILD_VERSION=${2:-0.os}
+BUILD_VERSION=${2:-0.go}
 MINGW64_PREFIX=/usr/x86_64-w64-mingw32
 PKG_DIR=`pwd`/build/win64/${DEBIAN_PKG_NAME}_${VERSION}-${BUILD_VERSION}_all
 
@@ -32,26 +32,26 @@ export CC=x86_64-w64-mingw32-gcc
 export CFLAGS="-O2 -g -pipe -Wall -fexceptions --param=ssp-buffer-size=4 -mms-bitfields"
 export CXX=x86_64-w64-mingw32-g++
 export CXXFLAGS="-O2 -g -pipe -Wall -fexceptions --param=ssp-buffer-size=4 -mms-bitfields"
-export PKG_CONFIG_PATH="/usr/x86_64-w64-mingw32/lib/pkgconfig:/usr/x86_64-w64-mingw32/share/pkgconfig"
+export PKG_CONFIG_PATH="$MINGW64_PREFIX/lib/pkgconfig:$MINGW64_PREFIX/share/pkgconfig"
 export LDFLAGS="-Wl,--exclude-libs=libintl.a -Wl,--exclude-libs=libiconv.a -Wl,--no-keep-memory -fstack-protector"
 
 ./configure --cache-file=mingw64-config.cache \
 	--host=x86_64-w64-mingw32 \
 	--build=x86_64-suse-linux-gnu \
 	--target=x86_64-w64-mingw32 \
-	--prefix=/usr/x86_64-w64-mingw32 \
-	--exec-prefix=/usr/x86_64-w64-mingw32 \
-	--bindir=/usr/x86_64-w64-mingw32/bin \
-	--sbindir=/usr/x86_64-w64-mingw32/sbin \
-	--sysconfdir=/usr/x86_64-w64-mingw32/etc \
-	--datadir=/usr/x86_64-w64-mingw32/share \
-	--includedir=/usr/x86_64-w64-mingw32/include \
-	--libdir=/usr/x86_64-w64-mingw32/lib \
-	--libexecdir=/usr/x86_64-w64-mingw32/libexec \
-	--localstatedir=/usr/x86_64-w64-mingw32/var \
-	--sharedstatedir=/usr/x86_64-w64-mingw32/com \
-	--mandir=/usr/x86_64-w64-mingw32/share/man \
-	--infodir=/usr/x86_64-w64-mingw32/share/info --enable-stl --enable-release --enable-debug --enable-accessibility \
+	--prefix=$MINGW64_PREFIX \
+	--exec-prefix=$MINGW64_PREFIX \
+	--bindir=$MINGW64_PREFIX/bin \
+	--sbindir=$MINGW64_PREFIX/sbin \
+	--sysconfdir=$MINGW64_PREFIX/etc \
+	--datadir=$MINGW64_PREFIX/share \
+	--includedir=$MINGW64_PREFIX/include \
+	--libdir=$MINGW64_PREFIX/lib \
+	--libexecdir=$MINGW64_PREFIX/libexec \
+	--localstatedir=$MINGW64_PREFIX/var \
+	--sharedstatedir=$MINGW64_PREFIX/com \
+	--mandir=$MINGW64_PREFIX/share/man \
+	--infodir=$MINGW64_PREFIX/share/info --enable-stl --enable-release --enable-debug --enable-accessibility \
 	--enable-vendor=ubuntu
 
 make $PARALLEL_PRMS
