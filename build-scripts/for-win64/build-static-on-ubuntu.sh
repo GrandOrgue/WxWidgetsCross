888744b2e99e2e0b05b8b62055ffdd76904e6fd2
larspalo <larspalo@yahoo.se>
2024-02-27 14:30:38 +0100

    Changed maintainer info and paths according to feedback on PR. Fixed warnings when running workflow file.

--- build-scripts/for-win64/build-static-on-ubuntu.sh
+++ build-scripts/for-win64/build-static-on-ubuntu.sh
@@ -8,14 +8,14 @@ DEBIAN_PKG_NAME=wxwidgets3-mingw-w64-static
 VERSION=${1:-3.2.3}
 BUILD_VERSION=${2:-0.go}
 MINGW64_PREFIX=/usr/x86_64-w64-mingw32
-PKG_DIR=`pwd`/build/win64/${DEBIAN_PKG_NAME}_${VERSION}-${BUILD_VERSION}_all
+PKG_DIR=`pwd`/build/win64-static/${DEBIAN_PKG_NAME}_${VERSION}-${BUILD_VERSION}_all
 
 
-mkdir -p build/win64
-rm -rf build/win64/*
-cp -Rv $SRC_DIR/submodules/WxWidgets build/win64/src
+mkdir -p build/win64-static
+rm -rf build/win64-static/*
+cp -Rv $SRC_DIR/submodules/WxWidgets build/win64-static/src
 
-pushd build/win64/src
+pushd build/win64-static/src
 
 ./configure --host=x86_64-w64-mingw32 --prefix=$MINGW64_PREFIX --enable-unicode --disable-shared
 
@@ -34,7 +34,7 @@ cat >$PKG_DIR/DEBIAN/control <<EOF
 Package: $DEBIAN_PKG_NAME
 Version: $VERSION-$BUILD_VERSION
 Architecture: all
-Maintainer: Oleg Samarin <osamarin68@gmail.com>
+Maintainer: Lars Palo <larspalo@yahoo.se>
 Description: Everything needed for development with wxWidgets/wxMSW
 EOF
 
